78a6da926e211a6d3f49d596caf22ce69b23229a
Caio Arruda <caioarruda@gmail.com>
2025-12-05 13:45:30 -0300

    ad

--- start-ollama.sh
+++ start-ollama.sh
@@ -6,8 +6,8 @@ ollama serve &
 # espera o server inicializar
 sleep 5
 
-echo "Baixando modelo llama3.1:3b-instruct-q4_K_M..."
-ollama pull llama3.1:3b-instruct-q4_K_M
+echo "Baixando modelo llama3:3b-instruct-q3_K_M..."
+ollama pull llama3:8b-instruct-q3_K_M
 
 echo "Modelo pronto. Mantendo o container ativo."
 tail -f /dev/null
